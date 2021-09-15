6517eca2bf81c09e990e2563dd63337d4cf68693
John Ellison <jellison@enterprisebank.com>
2021-09-15 12:01:53 -0500

    passing in an event argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,16 +5,18 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
   render() {
+    const product = "example argument";
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
